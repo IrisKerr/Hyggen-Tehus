961c08cec5e72af5afc2fdd1d07247a80659105f
IrisKerr <iris.kerr@hotmail.com>
2023-05-06 17:52:43 +0200

    workshops files added

--- router.js
+++ router.js
@@ -12,6 +12,21 @@ router.get('/workshops', (request, response) => {
     response.render('workshops.ejs', {workshops})
 });
 
+router.get('/workshops/:workshopDesc', (request, response) => {
+    workshopDescription = request.params.workshopDesc;
+
+    requestedWorkshop = workshops.find((workshop) => {
+        if (workshop.name === workshopDescription) { return true; }
+        else { return false; }
+    });
+
+     if (requestedWorkshop) {
+        response.render(`${requestedWorkshop.name}.ejs`, {workshops, workshopDescription})
+     }
+    
+    });
+    
+
 router.get('/teas', (request, response) => {
     response.render('teas.ejs')
 });
